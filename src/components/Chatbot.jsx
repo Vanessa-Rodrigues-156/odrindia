ca71906a5c1784caa83893b1afaefc45c6861e97
AnjaliSinghhhh <132354230+AnjaliSinghhhh@users.noreply.github.com>
2025-04-22 20:42:50 +0530

    Update Chatbot.jsx

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -48,13 +48,14 @@ const Chatbot = () => {
             backgroundColor: 'white'
           }}
         >
-          <iframe
-            src="https://console.dialogflow.com/api-client/demo/embedded/0f378c1a-0611-45d7-aa77-0faccc66742c"
-            width="100%"
-            height="100%"
-            style={{ border: 'none' }}
-            allow="microphone"
-          ></iframe>
+         <iframe
+  src="https://console.dialogflow.com/api-client/demo/embedded/0f378c1a-0611-45d7-aa77-0faccc66742c"
+  width="100%"
+  height="100%"
+  style="border: none;"
+  allow="microphone"
+></iframe>
+
         </div>
       )}
     </>
