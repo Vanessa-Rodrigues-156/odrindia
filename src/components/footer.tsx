20d456bd0753b745a77dcdd17087d9dc3de2258a
AnjaliSinghhhh <132354230+AnjaliSinghhhh@users.noreply.github.com>
2025-04-25 13:17:41 +0530

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -84,7 +84,7 @@ export default function Footer() {
             <ul className="space-y-3">
               <li className="flex items-start">
                 <MapPin className="mr-2 h-5 w-5 shrink-0 text-sky-400" />
-                <span className="text-gray-300">123 borivali Mumbai Maharashtra India </span>
+                <span className="text-gray-300">New:41 Old:44,M.G.Ramachandran Road,Besant Nagar,Chennai-600090. </span>
               </li>
               <li className="flex items-center">
                 <Phone className="mr-2 h-5 w-5 shrink-0 text-sky-400" />
@@ -92,7 +92,7 @@ export default function Footer() {
               </li>
               <li className="flex items-center">
                 <Mail className="mr-2 h-5 w-5 shrink-0 text-sky-400" />
-                <span className="text-gray-300">info@odrindia.com</span>
+                <span className="text-gray-300">contact@crekodr.com</span>
               </li>
             </ul>
           </div>
