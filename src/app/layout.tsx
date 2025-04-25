5e149b807fc0a78a3a837da860d48af4ac673dd4
Vanessa <vanessarodrigues010506@gmail.com>
2025-04-25 09:24:38 +0530

    build successfull

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ToastProvider } from '@/components/ui/use-toast'
-import Chatbot from "@/components/Chatbot";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 const geistSans = Geist({
